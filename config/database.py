f281c96d1915abacea0507195c07746e626257a9
danainan <71175110+danainan@users.noreply.github.com>
2023-03-01 17:02:14 +0700

    Update database.py

--- config/database.py
+++ config/database.py
@@ -9,7 +9,7 @@ ssl._create_default_https_context = ssl._create_unverified_context
 # client = MongoClient("mongodb+srv://adminjame:1234@cluster0.4ientud.mongodb.net/?retryWrites=true&w=majority", ssl=True, ssl_cert_reqs=ssl.CERT_NONE)
 
 # client = MongoClient("mongodb+srv://"+config.settings.user_name+":"+config.settings.pass_word+"@"+config.settings.host+"/?retryWrites=true&w=majority", ssl=True, ssl_cert_reqs=ssl.CERT_NONE)
-mongodb+srv://"+config.settings.username+":"+config.settings.password+"@"+config.settings.host+"/test
+client = MongoClient("mongodb+srv://"+config.settings.username+":"+config.settings.password+"@"+config.settings.host+"/test", ssl=True, ssl_cert_reqs=ssl.CERT_NONE)
 db = client.swe_classroom
 
 collection_name = db["course"]
