9f4bd0d90b6320c797a55621d2c7dae2a1e415b2
danainan <71175110+danainan@users.noreply.github.com>
2023-03-01 18:29:28 +0700

    Update database.py

--- config/database.py
+++ config/database.py
@@ -8,8 +8,8 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 # client = MongoClient("mongodb+srv://adminjame:1234@cluster0.4ientud.mongodb.net/?retryWrites=true&w=majority", ssl=True, ssl_cert_reqs=ssl.CERT_NONE)
 
-# client = MongoClient("mongodb+srv://"+config.settings.user_name+":"+config.settings.pass_word+"@"+config.settings.host+"/?retryWrites=true&w=majority", ssl=True, ssl_cert_reqs=ssl.CERT_NONE)
-client = MongoClient("mongodb+srv://"+config.settings.user_name+":"+config.settings.pass_word+"@"+config.settings.host+"/test", ssl=True, ssl_cert_reqs=ssl.CERT_NONE)
+client = MongoClient("mongodb+srv://"+config.settings.user_name+":"+config.settings.pass_word+"@"+config.settings.host+"/?retryWrites=true&w=majority", ssl=True, ssl_cert_reqs=ssl.CERT_NONE)
+# client = MongoClient("mongodb+srv://"+config.settings.user_name+":"+config.settings.pass_word+"@"+config.settings.host+"/test", ssl=True, ssl_cert_reqs=ssl.CERT_NONE)
 db = client.swe_classroom
 
 collection_name = db["course"]
